c2f385cd8ac472605ba581f7776da7570b0cff7c
Nathan Hoad <nathan@nathanhoad.net>
2018-03-02 10:15:07 +1100

    Fixed database naming

--- server/config.js
+++ server/config.js
@@ -1,11 +1,12 @@
 const Path = require('path');
 
 let config = {
-  DATABASE: `./server/${process.env.NODE_ENV}.sqlite`,
+  DATABASE: `./server/database.sqlite`,
   UPLOAD_PATH: Path.resolve(__dirname, '../uploads')
 };
 
 if (process.env.NODE_ENV === 'test') {
+  config.DATABASE = './server/test.sqlite';
   config.UPLOAD_PATH = '/tmp';
 }
 
