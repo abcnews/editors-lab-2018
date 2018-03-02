d2600f9f5d48e5c8f012d73704025ee4e246ed8c
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-03-02 12:19:51 +1100

    Sorry Nathan.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 const { h, Component } = require('preact');
+// const { BrowserRouter, Route, Switch } = require('react-router-dom');
 const { Router } = require('preact-router');
 const Home = require('../Home');
 const Project = require('../Project');
@@ -11,12 +12,28 @@ class App extends Component {
       <div className={styles.root}>
         <Router>
           <Home path="/" />
+          <ProjectInbox path="/inbox/:slug" />
           <Project path="/:slug" />
-          <ProjectInbox path="/:slug/inbox" />
         </Router>
       </div>
     );
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <div className={styles.root}>
+//           <Switch>
+//             <Route exact path="/" component={Home} />
+//             <Route path="/:slug/inbox" component={ProjectInbox} />
+//             <Route path="/:slug" component={Project} />
+//           </Switch>
+//         </div>
+//       </BrowserRouter>
+//     );
+//   }
+// }
 
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  FILE_TYPES: {
+    'photo-person': 'A photo of someone',
+    'photo-object': 'A photo of something'
+  },
+  UPLOADS_ROOT: '/uploads/'
+};
